55bd7bf241c08ec38f63b430a6418ac223c232a5
Matei-Marius Micu <micumatei@gmail.com>
2019-01-14 23:47:49 +0200

    Add ability to set prediction

--- _kubeless/dummy_server.py
+++ _kubeless/dummy_server.py
@@ -4,12 +4,32 @@ import os
 import json
 
 class HelloWorld(object):
+
+    DATA = 10
+
+    def get_resp(self):
+        resp = os.getenv("RESP", None)
+        if self.DATA is None:
+            return resp
+        return self.DATA
+
     @cherrypy.expose
     def index(self):
-        resp = os.getenv("RESP", None)
+        resp = self.get_resp()
         if resp != None:
             return json.dumps({"predicted": int(resp)})
         return open("resp").read()
+
+    @cherrypy.expose
+    def set(self, data):
+        try:
+            if data is not None:
+                self.DATA = int(data)
+        except Exception:
+            self.DATA = None
+        return "OK"
+                
+
 
 cherrypy.server.socket_host = '0.0.0.0'
 cherrypy.quickstart(HelloWorld())
